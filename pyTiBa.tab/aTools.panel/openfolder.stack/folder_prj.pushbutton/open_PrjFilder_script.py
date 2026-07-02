82b6967d2ab710e025cd288941ba7f7b067746cb
Tillmann Baumeister <tillmbaumeister@gmail.com>
2026-07-02 10:31:10 +0200

    Refactor to open Revit project folder directly
    Refactor script to open Revit project folder in Windows Explorer using subprocess. Removed old filepath handling and added checks for unsaved models.

--- pyTiBa.tab/aTools.panel/openfolder.stack/folder_prj.pushbutton/open_PrjFilder_script.py
+++ pyTiBa.tab/aTools.panel/openfolder.stack/folder_prj.pushbutton/open_PrjFilder_script.py
@@ -1,36 +1,29 @@
-'''Open Revit Family 2019 Folder in WIN Explorer'''
+'''Open Revit Prj Folder in WIN Explorer'''
 
 
 #__title__ = "Prj\nFolder" 
 __author__ = "T.Baumeister" 
 
-
 from Autodesk.Revit import DB 
 import sys, os 
 import traceback 
-#import pyrevit 
+import pyrevit 
 from pyrevit import (forms, revit) 
-
-
-# create filepath.txt file at first run.  Overwrite filepath when shiftclick! 
-def filepath(filename_str): 
-    path = os.path.split(sys.argv[0])[0] 
-    try: 
-        if __shiftclick__ == True: raise Exception() 
-        with open(path +"\\"+ filename_str, "r+") as f: # a+ mode, because I need "create file" funciton
-            folderpath = f.read() 
-            return folderpath     
-    except:                       
-        folderpath = forms.pick_folder() 
-        if not folderpath: sys.exit()    
-        with open(path + "\\filepath.txt", "w") as f: 
-            f.write(folderpath) 
-        return folderpath  
-
-
-path = filepath("filepath.txt") 
-pathtxt = 'explorer  "' + path + '"' 
-
 import subprocess
-subprocess.Popen(pathtxt)
+
+# Get the current Revit document path
+pathname = __revit__.ActiveUIDocument.Document.PathName
+
+if pathname:
+    # Extract the folder path
+    folderpath = os.path.split(pathname)[0]
+    # print("Opening folder: {}".format(folderpath))
+    
+    # Correct way to open a folder in Windows Explorer
+    subprocess.Popen(['explorer', folderpath])
+else:
+    pyrevit.forms.alert("Warning: This model hasn't been saved yet, so there is no folder to open!", title="Open Project Folder", warn_icon=True)
+    # print("Warning: This model hasn't been saved yet, so there is no folder to open!")
+
+
 
